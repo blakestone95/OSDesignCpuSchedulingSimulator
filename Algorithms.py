7ddd62deb696ec3ee93f00825f6aef48caf1f70f
blakestone95 <blakestone.trumpet1@gmail.com>
2016-05-23 20:03:25 -0700

    Added header comments and started attempting to code

---
+++ Algorithms.py
@@ -0,0 +1,5 @@
+# Algorithms.py
+# Author: Blake Sawyer
+# 
+# Create classes for CPU scheduling algorithms
+# 
---
+++ Processes.py
@@ -0,0 +1,9 @@
+# Processes.py
+# Author: Blake Sawyer
+#
+# Create process table and PCB
+
+class PCB:
+	
+
+class ProcTbl:
