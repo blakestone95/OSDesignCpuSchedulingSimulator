5914d35e6b0be9623608fe5385c44bd493582b9d
blakestone95 <blakestone.trumpet1@gmail.com>
2016-05-27 09:51:23 -0700

    Whoops didn't save the file

---
+++ Overlord.py
@@ -0,0 +1,15 @@
+# Overlord
+# Author: Blake Sawyer, Robert Berglund, Daniel Schiefer
+# 
+# Control process distribution, monitor cpu cores, manage queues, run simulation
+import Processes
+import Algorithms
+import cpuScheduler
+import Processors
+
+# correct my syntax if it's wrong...
+def Overlord():
+	# Start GUI
+	
+	# Return variables from GUI
+	
