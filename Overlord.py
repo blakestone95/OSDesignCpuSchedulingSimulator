f1a9d919671ecbf23182807a675b72bbe4a64c0e
cyberninja42 <bobby.berglund@gmail.com>
2016-06-05 13:04:59 -0700

    Updates

--- Overlord.py
+++ Overlord.py
@@ -7,6 +7,8 @@ import Algorithms
 import cpuScheduler
 import Processors
 
+currenttime = 0
+
 # correct my syntax if it's wrong...
 def Overlord(datafilereader):
     # Instead of Overlord starting everything, Overlord will be called by GUI 
@@ -14,3 +16,24 @@ def Overlord(datafilereader):
     for row in datafilereader:
         print(str(row))
     #print("Hello World!")
+
+    for processor in processors:
+        if processor.getstate() == "idle":
+            #Give something to do
+            processor.execute(pid,runtime)
+
+    #collect all times for comparison
+    times = []
+    #Get time from now to time of next arriving process
+    times.append(currenttime-waitqueue[0])
+    #Get remaining processing time for each processor
+    for processor in processors:
+        times.append(processor.currentruntime())
+
+    subtime = min(times)
+
+    #Need to decrement current time
+    for processor in processors:
+        processor.decrementtime(subtime)
+    currenttime += subtime
+    
