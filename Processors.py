8f3516ab48ad8c65048e283f0248d7726cc7f86f
cyberninja42 <bobby.berglund@gmail.com>
2016-06-05 15:31:46 -0700

    New Stuff
    Added halt and other things

--- Processors.py
+++ Processors.py
@@ -6,7 +6,8 @@ class CPU:
     #Initialize Variables
     state = "idle"      #State of processor
     twait = 0           #Time processor is idle
-    tactive = 0         #Time processor is running
+    tactive = 0         #Time processor is runnings
+    pid = 0
     processtime = 0     #Counter for processor to increment to burst time
     processslot = 0     #Place holder for burst time in list
     ProcTbl = None      #Process table 
@@ -16,27 +17,53 @@ class CPU:
         self.ProcTbl = ProcTbl
 
     #Set processor to execute a specified process from the pid
-    def execute(self,pid):
+    def execute(self,pid,runtime):
         index = self.ProcTbl.pid.index(pid)     #Get address of process in table
         PCB = self.ProcTbl.pcb[index]           #Get pcb from process table
 
         #Set process burst time to first in list
+        self.pid = pid
         self.processslot = 0
-        self.processtime = 0
+        self.processtime = runtime
         state = "running"   #Set processor to running state
+
+    #Set processor to execute a specified process from the pid
+    def halt(self):
+        state = "idle"   #Set processor to running state
+        return [self.pid,self.processtime]
 
     #Return processor usage value
     def usage(self):
         return tactive/(tactive+twait)
 
+    def getstate(self):
+        return self.state
+
+    def currentruntime(self):
+        return self.processtime
+
+    def decrementtime(self,subtime):
+        if state == "running":
+            self.tactive += subtime
+            self.processtime -= subtime
+            if self.processtime == 0:
+                state = "idle"
+                return pid
+            elif self.processtime < 0:
+                print("error CPU time < 0")
+        elif state == "idle":
+            self.twait += subtime
+            #print("error can't decrement, CPU is idle")
+        return 0
+
     #Loop function to regularly update cpu state
-    def loop(self):
+    """def loop(self):
         if state == "running":
             self.tactive += 1       #Increment active time
             self.processtime += 1   #Increment burst time and check if it is the final value
             if (self.processtime == PCB.tburst[self.processslot]):
                 if self.processslot/2 < (len(PCB.tburst)-1)/2:    #Move to the next burst slot if it exists
                     self.processslot += 2
-                    state = "blocked"       #Processor goes into blocked state since next slot is IO
+                    state = "idle"       #Processor goes into blocked state since next slot is IO
         else:
-            self.twait += 1         #If processor is not running increment wait time
+            self.twait += 1         #If processor is not running increment wait time"""
