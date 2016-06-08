16105e08b343db6e6535aa3e9d27deba153e49c2
cyberninja42 <bobby.berglund@gmail.com>
2016-06-07 17:05:11 -0700

    Add files via upload

--- CollectData.py
+++ CollectData.py
@@ -5,7 +5,7 @@
 #example.addtime(tarr,tfin)
 
 #Do this at the end
-#averageturnaroundtime = example.totaltime()
+#averageturnaroundtime = example.totaltime(length)
 
 class turnaroundtime:
         def __init__(self):
@@ -14,8 +14,8 @@ class turnaroundtime:
         def addtime(self,tarr,tfin):
                 self.times.append(tfin - tarr)
 
-        def totaltime(self):
-                return sum(self.times)
+        def totaltime(self,length):
+                return sum(self.times)/length
 
 #Do this to start
 #example = waittime()
@@ -24,7 +24,7 @@ class turnaroundtime:
 #example.addtime(tarr,tfin,tburst)
 
 #Do this at the end
-#averagewaittime = example.totaltime()
+#averagewaittime = example.totaltime(length)
 
 class waittime:
         def __init__(self):
@@ -33,8 +33,8 @@ class waittime:
         def addtime(self,tarr,tfin,tburstsum):
                 self.times.append((tfin - tarr) - tburstsum)
 
-        def totaltime(self):
-                return sum(self.times)
+        def totaltime(self,length):
+                return sum(self.times)/length
 
 
 #Do this to start
@@ -44,7 +44,7 @@ class waittime:
 #example.addtime(trep)
 
 #Do this at the end
-#averageresponsetime = example.totaltime()
+#averageresponsetime = example.totaltime(length)
 
 class responsetime:
         def __init__(self):
@@ -53,6 +53,6 @@ class responsetime:
         def addtime(self,trep):
                 self.times.append(trep)
 
-        def totaltime(self):
-                return sum(self.times)
+        def totaltime(self,length):
+                return sum(self.times)/length
 
